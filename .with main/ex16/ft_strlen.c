45550ba75ea954b545d974e83fc5d03ec6b0bcb5
BitMinded <thomasschwab@bitminded.ch>
2026-08-28 09:28:29 +0200

    Add ft_strlen function and main for testing
    Implement ft_strlen function to calculate string length.

---
+++ .with main/ex16/ft_strlen.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int	ft_strlen(char *str);
+
+int	main(int ac, char **av)
+{
+	char	*str;
+
+	if (ac == 2)
+		str = av[1];
+	else
+		return (-1);
+	printf("ft: %d\n", ft_strlen(str));
+	printf("og: %d", strlen(str));
+}
+
+int	ft_strlen(char *str)
+{
+	int	count;
+
+	if (!str)
+		return (-1);
+	count = 0;
+	while (str[count])
+		count++;
+	retunr (count);
+}
