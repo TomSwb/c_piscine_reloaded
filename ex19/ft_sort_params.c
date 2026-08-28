4376c65d2ad5fdd8ac666be5e3103e570bace7e4
TomSwb <thomasschwab@bitminded.ch>
2026-08-28 21:29:45 +0200

    adding sort params

---
+++ ex19/ft_sort_params.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tomswb <tomswb@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/28 20:58:06 by tomswb            #+#    #+#             */
+/*   Updated: 2026/08/28 21:29:14 by tomswb           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_strcmp(char *s1, char *s2);
+void	ft_print_params(char **av);
+
+int	main(int ac, char **av)
+{
+	int		i;
+	char	*temp;
+	
+	if (ac < 2)
+		return (-1);
+	else
+	{
+		i = 1;
+		while (i < ac - 1)
+		{
+			if (ft_strcmp(av[i], av[i + 1]) > 0)
+			{
+				temp = av[i];
+				av[i] = av[i + 1];
+				av[i + 1] = temp;
+				i = 1;
+			}
+			else
+				i++;
+		}
+	};
+	ft_print_params(av);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
+}
+
+void ft_print_params(char **av)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			ft_putchar(av[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+}
