c0721418b07d4827132c96d0544fe38bfd11e767
BitMinded <thomasschwab@bitminded.ch>
2026-08-31 10:37:24 +0200

    Add ft_count_if and ft_strchr functions
    Implement ft_count_if to count elements matching a condition.

---
+++ .with main/ex26/ft_count_if.c
@@ -0,0 +1,40 @@
+
+int	ft_count_if(char **tab, int (*f)(char *));
+int	ft_strchr(char *);
+
+int	main(int ac, char **av)
+{
+	(void) ac;
+	printf("%d", ft_count_if(av, ft_strchr));
+}
+
+
+int	ft_count_if(char **tab, int (*f)(char *))
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (tab[i])
+	{
+		if (f(tab[i]) == 1)
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+int	ft_strchr(char *s)
+{
+	char	c;
+
+	c = 'a';
+	while (*s)
+	{
+		if (*s == c)
+			return (1);
+		s++;
+	}
+	return (0);
+}
